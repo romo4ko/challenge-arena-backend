f80bb979fcbcf18f0512a5f63d7dcc92d4df4af1
deniskorbakov <korbakovd@gmail.com>
2024-10-26 22:51:55 +0300

    feat: add logic for update user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
+use App\DTO\Api\User\Request\UserUpdateDTO;
 use App\Models\User;
 use App\Services\Api\UserService;
+use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller
 {
@@ -19,8 +21,12 @@ class UserController extends Controller
         return User::query()->findOrFail($id)?->toArray();
     }
 
-    public function update()
+    public function update(int $id, UserUpdateDTO $userUpdateDTO): array|JsonResponse
     {
+        $user = User::query()->findOrFail($id);
+
+        return $this->userService->update($user, $userUpdateDTO);
+
     }
 
     public function team()
@@ -28,6 +34,10 @@ class UserController extends Controller
     }
 
     public function challenge()
+    {
+    }
+
+    public function achievement()
     {
     }
 }
