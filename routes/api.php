2559d5be16eb68bf1922d688bd29ed23d3548e9f
deniskorbakov <korbakovd@gmail.com>
2024-10-26 22:51:31 +0300

    feat: add routes

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,18 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register'])->name('auth.register');
 Route::post('auth', [AuthController::class, 'auth'])->name('auth.auth');
 
+Route::group(['middleware' => ['auth:sanctum']], static function () {
+    Route::group(['prefix' => 'users'], static function () {
+        Route::get('/{id}', [UserController::class, 'show'])->name('users.show');
+        Route::post('/{id}', [UserController::class, 'update'])->name('users.update');
+        Route::get('/{id}/teams', [UserController::class, 'team'])->name('users.team');
+        Route::get('/{id}/challenges', [UserController::class, 'challenge'])->name('users.challenge');
+        Route::get('/{id}/achievements', [UserController::class, 'achievement'])->name('users.achievement');
+    });
+});
